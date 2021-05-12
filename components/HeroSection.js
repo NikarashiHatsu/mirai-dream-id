3ff8cc0d9aa4f7ebdeb280a5260e4f927fb1f62e
NikarashiHatsu <yourlovelydev@gmail.com>
2021-05-12 22:19:55 +0700

    [chore](component): reduce hero section padding top

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -10,7 +10,7 @@ export default class HeroSection extends React.Component {
                 <Head>
                     <script src="https://cdn.rawgit.com/progers/pathseg/master/pathseg.js"></script>
                 </Head>
-                <div className="relative flex items-center w-full h-screen pt-16 text-white bg-gradient-to-br from-purple-800 to-purple-700">
+                <div className="relative flex items-center w-full h-screen pt-8 text-white bg-gradient-to-br from-purple-800 to-purple-700">
                     <div className="absolute top-0 left-0 w-full h-full">
                         <Particles params={ParticlesJsConfig} />
                     </div>
