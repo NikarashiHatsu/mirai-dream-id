803a0e00a113764ac849c72e70530987a175c485
NikarashiHatsu <yourlovelydev@gmail.com>
2021-05-16 00:53:00 +0700

    revision

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,8 +19,8 @@ export default function Navbar() {
                     <Link href="/staff">
                         <a className="mr-6 text-sm font-semibold text-white hover:underline">Staff</a>
                     </Link>
-                    <a href="https://studio.mirai-dream.com" className="mr-6 text-sm font-semibold text-white hover:underline">
-                        Studio
+                    <a href="https://studio.mirai-dream.com" target="_blank" className="mr-6 text-sm font-semibold text-white hover:underline">
+                        Studio (Coming Soon)
                     </a>
                     {/* Temporarily disabled */}
                     {/* <Link href="/legal">
