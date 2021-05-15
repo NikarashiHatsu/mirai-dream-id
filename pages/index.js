47309e2c400f4987b544e41488792d41b8aa1a99
NikarashiHatsu <yourlovelydev@gmail.com>
2021-05-16 00:27:57 +0700

    [chore](web): setup for studio

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,17 @@
-// NextJS Components
 import Head from 'next/head'
-
-// Custom Components
-import AboutSection from '../components/AboutSection'
-import ContactSection from '../components/ContactSection'
-import FoundersSection from '../components/FoundersSection'
-import HeroSection from '../components/HeroSection'
-import Navbar from '../components/Navbar'
-import TalentSection from '../components/TalentSection'
-import Footer from '../components/Footer'
 
 export default function Home() {
     return (
         <>
             <Head>
-                <title>Mirai Dream ID | Homepage</title>
+                <title>Mirai Dream Studio | Homepage</title>
                 <link rel="icon" href="/mirai_dream.webp" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Lexend&family=Varela+Round&display=swap" rel="stylesheet" />
             </Head>
-            <Navbar />
 
-            <div className="font-lexend">
-                <HeroSection />         {/* Done */}
-                <TalentSection />       {/* Done */}
-                <AboutSection />        {/* Done */}
-                <FoundersSection />     {/* Done */}
-                <ContactSection />      {/* Done */}
-                <Footer />              {/* Done */}
+            <div className="flex items-center justify-center w-screen h-screen font-lexend">
+                <h1 className="text-xl md:text-3xl lg:text-6xl">-COMING SOON-</h1>
             </div>
         </>
     )
