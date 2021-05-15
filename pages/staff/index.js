60a62ebe961821f914e3e118c345af1dc22d1dea
NikarashiHatsu <yourlovelydev@gmail.com>
2021-05-15 12:15:02 +0700

    [chore](section): remove email from staff component

--- pages/staff/index.js
+++ pages/staff/index.js
@@ -34,48 +34,41 @@ export default function Staff() {
                             <StaffCard
                                 image="/staff/isamu_riandi.webp"
                                 name="Isamu Riandi"
-                                role="Founder & CEO"
-                                email="isamuriandri12@gmail.com" />
+                                role="Founder & CEO" />
                             <StaffCard
                                 image="/staff/pratama_m_iqbal.webp"
                                 name="Pratama M. Iqbal"
-                                role="Vice CEO"
-                                email="pratamaabjm@gmail.com" />
+                                role="Vice CEO" />
                             <StaffCard
                                 image="/staff/fuad_fauzi.webp"
                                 name="Fuad Fauzi"
-                                role="Bendahara - Marketing"
-                                email="fuaddomon@gmail.com" />
+                                role="Bendahara - Marketing" />
                             <StaffCard
                                 image="/staff/seisou.webp"
                                 name="Seisou"
-                                role="Discord Admin"
-                                email="-email-" />
+                                role="Discord Admin" />
                             <StaffCard
                                 image="/staff/reru_pi.webp"
                                 name="Reru / Pi"
-                                role="Social Media Admin"
-                                email="fakhrrelraihanh@gmail.com" />
+                                role="Social Media Admin" />
                             <StaffCard
                                 image="/staff/dimas_budi.webp"
                                 name="Dimas Budi"
-                                role="Video Clipper / MV Editor"
-                                email="dimbud48@gmail.com" />
+                                role="Video Clipper / MV Editor" />
                             <StaffCard
                                 image="/staff/aghits_nidallah.webp"
                                 name="Aghits Nidallah"
                                 role="Head Web Developer"
-                                email="yourlovelydev@gmail.com" />
+                                facebook="https://facebook.com/irlnidallah"
+                                twitter="https://twitter.com/NikarashiHatsu" />
                             <StaffCard
                                 name="Renzuku"
-                                role="Web Dev, MV Editor, Layout Maker"
-                                email="shiinamashiro568@gmail.com" />
+                                role="Web Dev, MV Editor, Layout Maker" />
                             <div className="flex justify-center col-span-4">
                                 <StaffCard
                                     image="/staff/tama.webp"
                                     name="Tama"
-                                    role="Graphic Designer, Video Clipper"
-                                    email="daniputra.pr@gmail.com" />
+                                    role="Graphic Designer, Video Clipper" />
                             </div>
                         </div>
                     </div>
@@ -101,40 +94,17 @@ export default function Staff() {
                                     image="/staff/zetsu.webp"
                                     className="text-gray-700"
                                     name="Zetsu"
-                                    role="Head Quarter Mirai Dream Studio"
-                                    email="m.rafly.indrawan@gmail.com" />
+                                    role="Head Quarter Mirai Dream Studio" />
                                 <StaffCard
                                     image="/staff/fatah.webp"
                                     className="text-gray-700"
                                     name="Fatah"
-                                    role="Vice Head Quarter Mirai Dream Studio"
-                                    email="alfatahmin9@gmail.com" />
+                                    role="Vice Head Quarter Mirai Dream Studio" />
                                 <StaffCard
                                     className="text-gray-700"
                                     name="EiShaa"
-                                    role="Vice Head Quarter Mirai Dream Studio"
-                                    email="eishashimera@gmail.com" />
+                                    role="Vice Head Quarter Mirai Dream Studio" />
                             </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="relative flex items-center w-full min-h-screen pt-20 pb-10 text-white bg-blue-300">
-                    {/* Top Separator */}
-                    <div className="absolute right-0 w-full h-8 bg-yellow-200 -top-8 transform-gpu skew-y-2"></div>
-                    <div className="absolute right-0 w-full h-8 bg-yellow-100 -top-4 transform-gpu skew-y-1"></div>
-                    <div className="absolute top-0 right-0 w-full h-8 bg-blue-200"></div>
-                    <div className="absolute right-0 w-full h-8 bg-blue-300 top-4 transform-gpu -skew-y-1"></div>
-
-                    <div className="absolute top-0 left-0 z-0 w-full h-full">
-                        <Particles params={ParticlesJsConfig} />
-                    </div>
-                    <div className="static top-0 z-10 flex flex-col w-full mx-auto max-w-7xl">
-                        <h2 className="text-4xl text-center text-white">
-                            Bagan Struktur Mirai Dream
-                        </h2>
-                        <div className="mx-auto mt-10 max-w-7xl">
-                            <img className="max-h-screen bg-gray-100 border border-black rounded" src="/bagan_mirai_dream.webp" />
                         </div>
                     </div>
                 </div>
