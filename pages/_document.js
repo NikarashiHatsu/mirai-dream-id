acbfc052cad221de5ead590688c509cd41332693
NikarashiHatsu <yourlovelydev@gmail.com>
2021-05-18 17:57:14 +0700

    removing page_path

--- pages/_document.js
+++ pages/_document.js
@@ -18,9 +18,7 @@ export default class MiraiDocument extends Document {
                                 function gtag() { dataLayer.push(arguments);}
 
                                 gtag('js', new Date());
-                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                                    page_path: window.location.pathname,
-                                });
+                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                             `,
                         }}
                     />
